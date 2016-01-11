5ae150fe7ca29061e629c5fe2deee06b0f08a136
邹邹邹雨 <1154837100@qq.com>
2016-01-11 19:50:31 +0800

    主页

---
+++ WLZ/FirstPage/Controller/WLZ_Dance_detailViewController.m
@@ -0,0 +1,37 @@
+//
+//  WLZ_Dance_detailViewController.m
+//  WLZ
+//
+//  Created by 邹邹邹雨 on 16/1/11.
+//  Copyright © 2016年 lwz. All rights reserved.
+//
+
+#import "WLZ_Dance_detailViewController.h"
+
+@interface WLZ_Dance_detailViewController ()
+
+@end
+
+@implementation WLZ_Dance_detailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ WLZ/FirstPage/Model/WLZ_DanceTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  WLZ_DanceTableViewCell.h
+//  WLZ
+//
+//  Created by 邹邹邹雨 on 16/1/11.
+//  Copyright © 2016年 lwz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WLZ_DanceTableViewCell : UITableViewCell
+@property (nonatomic, retain) UIImageView *imageV;
+@property (nonatomic, retain) UILabel *titleL;
+@property (nonatomic, retain) UILabel *personL;
+@property (nonatomic, retain) UIView *view;
+@property (nonatomic, retain) CAGradientLayer *gradientLayer;
+@end
