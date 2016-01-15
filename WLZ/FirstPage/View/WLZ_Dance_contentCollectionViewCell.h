d5d05f4447d4bfa2ad2d3cba85a5632ec7dcebdd
邹邹邹雨 <1154837100@qq.com>
2016-01-15 11:41:23 +0800

    16.1.15.11:40

--- WLZ/FirstPage/View/WLZ_Dance_contentCollectionViewCell.h
+++ WLZ/FirstPage/View/WLZ_Dance_contentCollectionViewCell.h
@@ -7,7 +7,31 @@
 //
 
 #import <UIKit/UIKit.h>
-
+@class WLZ_Dance_ListModel;
 @interface WLZ_Dance_contentCollectionViewCell : UICollectionViewCell
+@property (nonatomic, retain) UILabel *titleL;
+@property (nonatomic, retain) UILabel *catalogL;
+@property (nonatomic, retain) UILabel *catalogthemrL;
+@property (nonatomic, retain) UILabel *productL;
+@property (nonatomic, retain) UILabel *productthemeL;
+@property (nonatomic, retain) UILabel *purposeL;
+@property (nonatomic, retain) UILabel *purposethemeL;
+@property (nonatomic, retain) UILabel *suitableL;
+@property (nonatomic, retain) UILabel *suitablethemeL;
+@property (nonatomic, retain) UILabel *groupL;
+@property (nonatomic, retain) UILabel *groupthemeL;
+
+@property (nonatomic, retain) UILabel *genderL;
+@property (nonatomic, retain) UILabel *genderthemeL;
+
+@property (nonatomic, retain) UILabel *summaryL;
+@property (nonatomic, retain) UILabel *summarythemeL;
+
+@property (nonatomic, retain) UIScrollView *scrollV;
+
+@property (nonatomic, retain) WLZ_Dance_ListModel *wlzDance;
+
++ (CGFloat)heightWithStr:(NSString *)str;
+
 
 @end
