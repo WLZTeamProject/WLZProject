f7d130eddba8b69821393a0b9491605aa6c06966
왕닝 <1130728816@qq.com>
2016-01-18 17:24:19 +0800

    微信分享完成

---
+++ WLZ/SecondPage/View/WLZ_LunBo_View.h
@@ -0,0 +1,13 @@
+//
+//  WLZ_LunBo_View.h
+//  WLZ
+//
+//  Created by 왕닝 on 16/1/18.
+//  Copyright © 2016年 lwz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WLZ_LunBo_View : UIView
+
+@end
---
+++ WLZ/SecondPage/View/WLZ_LunBo_View.m
@@ -0,0 +1,21 @@
+//
+//  WLZ_LunBo_View.m
+//  WLZ
+//
+//  Created by 왕닝 on 16/1/18.
+//  Copyright © 2016年 lwz. All rights reserved.
+//
+
+#import "WLZ_LunBo_View.h"
+
+@implementation WLZ_LunBo_View
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
